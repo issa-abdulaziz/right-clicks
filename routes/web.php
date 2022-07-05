10f4ff769bc5be81e3fd1e2e80de3ab2a0c2bb1e
issa-abdulaziz <issaabdulaziz20919@gmail.com>
2022-07-05 19:06:20 +0300

    hadling redirect after login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/redirect-after-login', function () {
+        // if (auth()->user()->is_admin)
+        //     return redirect('/admin/task');
+        // return redirect('/user/task');
+        if (auth()->user()->is_admin)
+            return 'admin';
+        return 'user';
+    });
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'is_admin'], function () {
+
+    });
+
+    Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
+
+    });
+
 });
