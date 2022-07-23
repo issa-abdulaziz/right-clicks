aed0d8de387ea28db49f9e0f871d9d2e74bdaba5
issa-abdulaziz <issaabdulaziz20919@gmail.com>
2022-07-23 20:45:13 +0300

    set website icon for login and register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" type="image/png" href="{{ asset('images/goal_127px.png') }}"/>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
